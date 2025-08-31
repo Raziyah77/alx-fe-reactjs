1f528fc4ec0a1da3aa0ac9a2b7cf77df2e74d2a9
Raziyah Mwihaki <raziyahmwihaki@gmail.com>
2025-08-31 12:20:56 +0300

    Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,26 +1,17 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
-    </div>
-  );
-}
-
-export default App;
-
-
-
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <div>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </div>
   );
