5af2ea204ad072598b73db569ca220289d583d37
Raziyah Mwihaki <raziyahmwihaki@gmail.com>
2025-09-06 11:21:59 +0300

    moved userContext into components folder

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from '../UserContext';  // adjust path if needed
+import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
