174f552807367c6168fa796fabebeb980cbd397c
Raziyah Mwihaki <raziyahmwihaki@gmail.com>
2025-09-06 11:29:17 +0300

    Modified Userprofile to fix error

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
-const UserProfile = (props) => {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{userData.name}</h2>
+      <p>{userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
