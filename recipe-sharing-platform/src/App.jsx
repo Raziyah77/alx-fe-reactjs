58c2476e4b81bd4556fd295839429e6fc0ce1c60
Raziyah Mwihaki <raziyahmwihaki@gmail.com>
2025-10-04 20:11:20 +0300

    Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <div className="text-blue-500 text-3xl font-bold text-center mt-10">
-      Tailwind CSS is working!
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
     </div>
   );
 }
